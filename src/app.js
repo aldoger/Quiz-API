cbb34ea4ae906f5951dffe6e99eba5b6de0de056
Geraldo Benjamin Nainggolan <nainggolanben12@gmail.com>
2025-01-04 01:18:13 +0700

    finish dasprog quiz

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
 import express from "express";
+import { fileURLToPath } from 'url';
+import path from 'path';
 import router from "./routes/router.js"
+
+// Recreate __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 
+app.use('/asset', express.static(path.join(__dirname, 'images')));
 app.use('/', router);
+
+app.get('/get-image/:imageName', (req, res) => {
+    const { imagename } = req.params;
+    const imagePath = path.join(__dirname, 'images', imagename);
+
+    res.sendFile(imagePath, (err) => {
+        if(err){
+            res.status(404).send('Image not found');
+        }
+    });
+});
 
 export default app;
--- src/models/quiz.js
+++ src/models/quiz.js
@@ -1,6 +1,6 @@
 export const quizDasprog = [
     {
-        title: "Apa output dari program ini",
+        question: "Apa output dari program ini?",
         opt: {
             opt1: {
                 text: "0",
@@ -15,11 +15,81 @@ export const quizDasprog = [
                 value: false,
             }
         }
+    },    
+    {
+        question: "Apa fungsi dari header ini?",
+        opt: {
+            opt1: {
+                text: "0",
+                value: true,
+            },
+            opt2: {
+                text: "hello world",
+                value: false,
+            },
+            opt3: {
+                text: "error",
+                value: false,
+            }
+        }
+    },
+    {
+        question: "Fungsi untuk menulis 'Hello World!'",
+        opt: {
+            opt1: {
+                text: "print('hello world')",
+                value: false,
+            },
+            opt2: {
+                text: `prinf("Hello World!")`,
+                value: true,
+            },
+            opt3: {
+                text: "console.log(Hello world!)",
+                value: false,
+            }
+        }
+    },
+    {
+        question: "Tipe data apa yang digunakan untuk bilangan bulat?",
+        opt: {
+            opt1: {
+                text: "double",
+                value: false,
+            },
+            opt2: {
+                text: "float",
+                value: false,
+            },
+            opt3: {
+                text: "int",
+                value: true,
+            }
+        }
+    },
+    {
+        question: "Perintah apa yang digunakan untuk menginput data?",
+        opt: {
+            opt1: {
+                text: "printf()",
+                value: false,
+            },
+            opt2: {
+                text: "scanf()",
+                value: true,
+            },
+            opt3: {
+                text: "read()",
+                value: false,
+            }
+        }
     }
 ]
 
 export const quizSisdig = [
-
+    {
+        question: ""
+    }
 ]
 
 export const quizPPL = [
