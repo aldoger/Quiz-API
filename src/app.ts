606f9e6c62f62ef2302f08a28981421b5cc2ca2f
Geraldo Benjamin Nainggolan <nainggolanben12@gmail.com>
2025-06-12 22:50:36 +0700

    update: update cors origin

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,8 @@ const app = express();
 app.use(
     cors(
         {
-            origin: `https://kode-kreasi.vercel.app/`,
-            credentials: true
+            origin: ["http://localhost:3000", "https://kode-kreasi.vercel.app"],
+            credentials: true,
         }
     )
 );
